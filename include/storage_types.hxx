953b5e0421c8021ec562fd5e4c0cbcc06b78eae7
PiotrGalazka <piotrgalazka@student.agh.edu.pl>
2026-01-21 14:10:30 +0100

    zaimplementowanie klas IPackageQueue, PackageQueue oraz IPackageStockpile

--- include/storage_types.hxx
+++ include/storage_types.hxx
@@ -1,8 +1,61 @@
+#include "package.hxx"
+
 enum class PackageQueueType {
     FIFO,
     LIFO
 };
 
-class IPackageStockpile {
+class IPackageStockpile{
+    public:
+    virtual void push(Package&& package) = 0;
 
+    virtual bool empty() const = 0;
+
+    virtual size_t size() const = 0;
+
+    virtual  ~IPackageStockpile()=default;
+
+    virtual Package begin() = 0;
+
+    virtual Package end() = 0;
+
+    virtual Package cbegin() const  = 0;
+
+    virtual Package cend() const = 0;
+
+};
+
+class IPackageQueue:IPackageStockpile{
+    public:
+
+    virtual Package pop() =0;
+
+    virtual PackageQueueType get_queue_type() const =0;
+
+    ~IPackageQueue() override = default;
+
+};
+
+class PackageQueue{
+    public:
+
+    PackageQueue() = delate;
+
+    PakageQueue(PackageQueueType QueueType) : QueueType_(QueueType), PackageList_(){};
+
+    void push(Package&& package) override {PackageList_.pushback(package);};
+
+    bool empty() const override{return if(PackageList_ == []);};
+
+    size_t size() const override {return PackageList_.size()};
+
+    Package pop() override {return PackageList_[-1];};
+
+    PackageQueueType const override {return QueueType_;};
+
+    ~IPackageQueue() override = default;
+
+    private:
+    PackageQueueType QueueType_;
+    std::list<Package> PackageList_;
 };
